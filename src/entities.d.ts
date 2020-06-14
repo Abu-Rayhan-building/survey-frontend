80b888612a030763926b76955135719c5daa6518
hosseinmp76 <hosseinmp76@viratech.ir>
2020-06-14 14:22:02 +0430

    adding interfaces

---
+++ src/entities.d.ts
@@ -0,0 +1,68 @@
+interface User {
+    name: string;
+    roles: Role[];
+}
+
+interface Role {
+    name: string;
+}
+
+interface Course{
+    Semester;
+    instructors: User[];
+    students: User[];
+    teachingAssistance: User[];
+}
+
+interface Semester {
+    name: String;
+}
+
+interface Survey {
+    course: Course;
+    name: String;
+    begging: Date;
+    endTime: Date;
+    questions: Question[];
+}
+
+type Question = RangedOptionQuestion|MultiChoiceQuestion|TextQuestion;
+
+private interface BaseQuestion {
+    text: string;
+    course: Course;
+}
+
+interface RangedOptionQuestion extends BaseQuestion {
+    min: number;
+    max: number;
+}
+
+interface MultiChoiceQuestion extends BaseQuestion{
+    options: string[];
+    forcedNumber: number;
+    availableNumber: number;
+}
+
+interface TextQuestion  extends BaseQuestion{
+    maxLength: number;
+}
+
+type Reply = RangedOptionReply|MultiChoiceReply|TextReply;
+
+private interface BaseReply{
+    question: Question;
+    responder: User;
+}
+
+interface MultiChoiceReply extends BaseReply{
+    optionIndex: number[];
+}
+
+interface TextReply extends BaseReply{
+    text: string;
+}
+
+interface RangedOptionReply extends BaseReply{
+    value: number;
+}
